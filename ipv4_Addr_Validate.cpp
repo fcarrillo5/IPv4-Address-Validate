345e298f2a18b7c6ef612692ad957aa5828be0ac
Freddy Carrillo <fcarrillo.linux@gmail.com>
2023-02-04 23:31:10 -0800

    Modify comments
    Minor change. Modified/beautify comments.
    
    Signed-off-by: Freddy Carrillo

--- ipv4_Addr_Validate.cpp
+++ ipv4_Addr_Validate.cpp
@@ -1,13 +1,15 @@
-//============================================================================
-// Name        : ipv4_Addr_Validate
-// Author      : Freddy Carrillo
-// Version     : 1.0
-// Date        : 17 August 2020
-// Description : Basic program that checks / validates IPv4 address.
-//               N1.N2.N3.N4 network address. 
-//               Constrain 0 <= N <= 255. For example, 192.168.88.1 displays
-//               VALID. For 192.168.10.256 displays INVALID
-//============================================================================
+/*
+ * ============================================================================
+ * Name        : ipv4_Addr_Validate
+ * Author      : Freddy Carrillo
+ * Version     : 1.0
+ * Date        : 17 August 2020
+ * Description : Basic program that checks / validates IPv4 address.
+ *               N1.N2.N3.N4 network address.
+ *               Constrain 0 <= N <= 255. For example, 192.168.88.1 displays
+ *               VALID. For 192.168.10.256 displays INVALID
+ * ============================================================================
+ */
 
 #include <stdio.h>
 #include <string.h>
@@ -15,37 +17,42 @@
 
 int main()
 {
-	char	str[16];		// IPv4 characters
-	char 	*tok;			// tokens
-	int  	k;				// holds conversion from string to int
-	int 	cntr = 0;		// counter for each 8-bit section in IP addr
+	char	str[16];	/* IPv4 characters */
+	char 	*tok;		/* tokens */
+	int  	k;		/* holds conversion from string to int */
+	int 	cntr = 0;	/* counter for each 8-bit section in IP addr */
 
-	printf("Enter IP address: ");	// Input IPv4
+	printf("Enter IP address: ");
 	fflush(stdout);
 	scanf("%15s", str);
 
-	tok = strtok(str, ".");			// get first token
+	/* get first token */
+	tok = strtok(str, ".");
 
 	while (tok != NULL)
 	{
 		printf("<<%s>>\n", tok);
-		k = atoi(tok);				// convert to int
-		if ((k >= 0) && (k <= 255)) // validate (0 <= n <= 255)
+		/* convert to int */
+		k = atoi(tok);
+		/* validate (0 <= n <= 255) */
+		if ((k >= 0) && (k <= 255))
 		{
 			cntr++;
-			if (cntr == 4)			// if pass all four network sections
+			/* if pass all four network sections */
+			if (cntr == 4)
 			{
-				printf("\nVALID\n\n"); // display valid
+				printf("\nVALID\n\n");
 				break;
 			}
 		}
 		else
 		{
-			printf("\nINVALID\n\n"); // display invalid otherwise
+			printf("\nINVALID\n\n");
 			break;
 		}
 
-		tok = strtok(NULL, ".");	// get next token until NULL
+		/* get next token until NULL */
+		tok = strtok(NULL, ".");
 	}
 
     return 0;
